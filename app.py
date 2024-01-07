b65ea81cb9f0428676b1ae71e1ff812fc27ed6cb
createdshots <jacktolley.1007@gmail.com>
2024-01-07 22:34:19 +0000

    Added node to the project

--- app.py
+++ app.py
@@ -15,23 +15,43 @@ labels = []
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    prediction = ''
     if request.method == 'POST':
         email = request.form['email']
-        label = request.form['label']
-        emails.append(email)
-        labels.append(label)
+        action = request.form.get('action')  # Use get() instead of []
 
-        # Train the classifier
-        X = vectorizer.fit_transform(emails)
-        classifier.fit(X, labels)
+        if action == 'Train':
+            label = request.form['label']
+            emails.append(email)
+            labels.append(label)
 
-        # Save the trained model and vectorizer
-        with open('model.pkl', 'wb') as f:
-            pickle.dump(classifier, f)
-        with open('vectorizer.pkl', 'wb') as f:
-            pickle.dump(vectorizer, f)
+            # Train the classifier
+            X = vectorizer.fit_transform(emails)
+            classifier.fit(X, labels)
 
-    return render_template('index.html')
+            # Save the trained model and vectorizer
+            with open('model.pkl', 'wb') as f:
+                pickle.dump(classifier, f)
+            with open('vectorizer.pkl', 'wb') as f:
+                pickle.dump(vectorizer, f)
+
+        elif action == 'Predict':
+            # Load the trained model and vectorizer
+            with open('model.pkl', 'rb') as f:
+                classifier = pickle.load(f)
+            with open('vectorizer.pkl', 'rb') as f:
+                vectorizer = pickle.load(f)
+
+            # Transform the email text to vector
+            email_vector = vectorizer.transform([email])
+            # Make a prediction
+            prediction = classifier.predict(email_vector)
+            if prediction == ['spam']:
+                prediction = 'This email is spam.'
+            else:
+                prediction = 'This email is not spam.'
+
+    return render_template('index.html', prediction=prediction)
 
 if __name__ == '__main__':
     app.run(debug=True)
